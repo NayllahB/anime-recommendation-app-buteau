a3de36cd212d3d083e371e6ac093afa94582f198
Nayllah Buteau <nayllah_buteau@outlook.com>
2025-07-25 17:23:17 -0400

    comments uploaded to understand starter code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,8 +3,13 @@ const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
 
 // admin login/logout
+//When user submits login, the login function in controllers.auth is called
 router.post("/login", controllers.auth.login);
+
+//When user clicks logout, logout function in controllers.auth is called
 router.get("/logout", controllers.auth.logout);
+
+//When user clicks sign up, create function in controlers.create is called
 router.post("/signup", controllers.user.create);
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,20 +2,32 @@ const router = require("express").Router();
 const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
 
+//GET homepage
+// Shows home page (content depend on if user is logged in)
 router.get("/", ({ session: { isLoggedIn } }, res) => {
   res.render("index", { isLoggedIn });
 });
 
+//GET Login page
 router.get("/login", async (req, res) => {
+  //If user is logged in, redirect to homepage
   if (req.session.isLoggedIn) return res.redirect("/");
+
+  //Show login page (user not logged in)
   res.render("login", { error: req.query.error });
 });
 
+//GET Sign up page
 router.get("/signup", async (req, res) => {
+  //If user is logged in, redirect to hompage
   if (req.session.isLoggedIn) return res.redirect("/");
+
+  //Show sign up page (user not logged in)
   res.render("signup", { error: req.query.error });
 });
 
+//GET protected page
+//Only renders protected page if user is logged in
 router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
   res.render("protected", { isLoggedIn });
 });
